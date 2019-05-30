07c1b54ffe88d31baea4620e005bc1125d756b77
Rafael Lamarques <31668776+rlamarques@users.noreply.github.com>
2019-05-30 13:54:45 -0300

    Copy code from CoCalc

--- relations.py
+++ relations.py
@@ -1,27 +1,52 @@
-keyC = ('C', ['C', 'Dm', 'Em', 'F', 'G', 'Am', 'Bm'])
-keyCs = ('C#', ['C#', 'D#m', 'Fm', 'F#', 'G#', 'A#m', 'Cm'])
-keyD = ('D', ['D', 'Em', 'F#m', 'G', 'A', 'Bm', 'C#m'])
-keyDs = ('D#', ['D#', 'Fm', 'Gm', 'G#', 'A#', 'Cm', 'Dm'])
-keyE = ('E', ['E', 'F#m', 'G#m', 'A', 'B', 'C#m', 'D#m'])
-keyF = ('F', ['F', 'Gm', 'Am', 'A#', 'C', 'Dm', 'Em'])
-keyFs = ('F#', ['F#', 'G#m', 'A#m', 'B', 'C#', 'D#m', 'Fm'])
-keyG = ('G', ['G', 'Am', 'Bm', 'C', 'D', 'Em', 'F#m'])
-keyGs = ('G#', ['G#', 'A#m', 'Cm', 'C#', 'D#', 'Fm', 'Gm'])
-keyA = ('A', ['A', 'Bm', 'C#m', 'D', 'E', 'F#m', 'G#m'])
-keyAs = ('A#', ['A#', 'Cm', 'Dm', 'D#', 'F', 'Gm', 'Am'])
-keyB = ('B', ['B', 'C#m', 'D#m', 'E', 'F#', 'D#m', 'A#m'])
+C  = ('C', ['C', 'E', 'G'])
+Cs = ('C#', ['C#', 'F', 'G#'])
+D  = ('D', ['D', 'F#', 'A'])
+Ds = ('D#', ['D#', 'G', 'A#'])
+E  = ('E', ['E', 'G#', 'B'])
+F  = ('F', ['F', 'A', 'C'])
+Fs = ('F#', ['F#', 'A#', 'C#'])
+G  = ('G', ['G', 'B', 'D'])
+Gs = ('G#', ['G#', 'C', 'D#'])
+A  = ('A', ['A', 'C#', 'E'])
+As = ('A#', ['A#', 'D', 'F'])
+B  = ('B', ['B', 'D#', 'F#'])
 
-harmonic_fields = {
-    'C' : keyC,
-    'C#': keyCs,
-    'D' : keyD,
-    'D#': keyD,
-    'E' : keyE,
-    'F' : keyF,
-    'F#': keyFs,
-    'G' : keyG,
-    'G#': keyGs,
-    'A' : keyA,
-    'A#': keyAs,
-    'B' : keyB
+Cm  = ('C', ['C', 'D#', 'G'])
+Csm = ('C#', ['C#', 'E', 'G#'])
+Dm  = ('D', ['D', 'F', 'A'])
+Dsm = ('D#', ['D#', 'F#', 'A#'])
+Em  = ('E', ['E', 'G', 'B'])
+Fm  = ('F', ['F', 'G#', 'C'])
+Fsm = ('F#', ['F#', 'A', 'C#'])
+Gm  = ('G', ['G', 'A#', 'D'])
+Gsm = ('G#', ['G#', 'B', 'D#'])
+Am  = ('A', ['A', 'C', 'E'])
+Asm = ('A#', ['A#', 'C#', 'F'])
+Bm  = ('B', ['B', 'D', 'F#'])
+
+available_chords = {
+    'A': A,
+    'A#': As,
+    'Am': Am,
+    'A#m': Asm,
+    'B': B,
+    'Bm': Bm,
+    'C': C,
+    'C#': Cs,
+    'Cm': Cm,
+    'C#m': Csm,
+    'D': D,
+    'D#': Ds,
+    'Dm': Dm,
+    'D#m': Dsm,
+    'E': E,
+    'Em': Em,
+    'F': F,
+    'F#': Fs,
+    'Fm': Fm,
+    'F#m': Fsm,
+    'G': G,
+    'G#': Gs,
+    'Gm': Gm,
+    'G#m': Gsm
 }
